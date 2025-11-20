3a4ddc722e23a840b90a7d5b0a0d496cf28b1ff7
JosefinaBacci <josefinabacci015@gmail.com>
2025-11-20 15:41:09 -0300

    Adding endpoints for ratings and opinions service in API Gateway

--- ApiGateway/config.js
+++ ApiGateway/config.js
@@ -3,4 +3,6 @@ export const {
   SECRET_JWT_KEY,
   AUTH_ENDPOINT = "http://auth:3001",
   USER_MANAGE_ENDPOINT = "http://users:3000",
+  RANKING_ENDPOINT = "http://calificacion:4000",
+  OPINIONS_ENDPOINT = "http://opiniones:4100",
 } = process.env;
--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import debug from "debug";
-import { AUTH_ENDPOINT, PORT, USER_MANAGE_ENDPOINT } from "./config.js";
+import { AUTH_ENDPOINT, PORT, USER_MANAGE_ENDPOINT, RANKING_ENDPOINT, OPINIONS_ENDPOINT } from "./config.js";
 import proxy from "express-http-proxy";
 import { verifyJWT } from "./middlewares/auth.middleware.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", proxy(AUTH_ENDPOINT));
 app.use("/user", [verifyJWT, proxy(USER_MANAGE_ENDPOINT)]);
+app.use("/ratings", verifyJWT, proxy(RANKING_ENDPOINT));
+app.use("/opinions/user", verifyJWT, proxy(OPINIONS_ENDPOINT));
+app.use("/opinions", proxy(OPINIONS_ENDPOINT));
 app.use(errorMiddleware);
 app.listen(PORT);
 
