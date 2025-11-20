c9d5ed1840282ee7b0a1942b1a682dd6be884e2c
Roman <jromanbrugno@gmail.com>
2025-11-20 19:56:50 -0300

    Feat: connect random movies services

--- ApiGateway/config.js
+++ ApiGateway/config.js
@@ -1,9 +1,10 @@
 export const {
   PORT = 3000,
   SECRET_JWT_KEY,
-  MONGO_URL = "mongodb://mongo:27017/moviesdb",
-  AUTH_ENDPOINT = "http://auth:3001",
-  USER_MANAGE_ENDPOINT = "http://users:3000",
-  RANKING_ENDPOINT = "http://calificacion:4000",
-  OPINIONS_ENDPOINT = "http://opiniones:4100",
+  MONGO_URL = "mongodb://mongo:27017/moviesdb/",
+  AUTH_ENDPOINT = "http://auth:3001/",
+  USER_MANAGE_ENDPOINT = "http://users:3000/",
+  RANKING_ENDPOINT = "http://calificacion:4000/",
+  OPINIONS_ENDPOINT = "http://opiniones:4100/",
+  RANDOM_MOVIES_ENDPOINT = "http://random-movies:3004/",
 } = process.env;
--- ApiGateway/index.js
+++ ApiGateway/index.js
@@ -6,7 +6,7 @@ import {
   PORT,
   USER_MANAGE_ENDPOINT,
   RANKING_ENDPOINT,
-  OPINIONS_ENDPOINT,
+  RANDOM_MOVIES_ENDPOINT,
 } from "./config.js";
 import proxy from "express-http-proxy";
 import { verifyJWT } from "./middlewares/auth.middleware.js";
@@ -20,6 +20,14 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", proxy(AUTH_ENDPOINT));
 app.use("/user", [verifyJWT, proxy(USER_MANAGE_ENDPOINT)]);
+app.use(
+  "/random",
+  proxy(RANDOM_MOVIES_ENDPOINT, {
+    proxyReqPathResolver: (req) => {
+      return `${RANDOM_MOVIES_ENDPOINT}random${req.url}`;
+    },
+  })
+);
 app.use(
   "/ratings",
   verifyJWT,
