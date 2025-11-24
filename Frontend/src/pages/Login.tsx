3211f1560c6f5ead10e25578f31cfc8b429806ad
Roman <jromanbrugno@gmail.com>
2025-11-24 10:19:38 -0300

    Feat: install tailwind and heroui

---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import { Input } from "@heroui/input";
+
+function Login() {
+  return (
+    <main>
+      <section>
+        <form action="">
+          <Input label="email" type="email" />
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default Login;
